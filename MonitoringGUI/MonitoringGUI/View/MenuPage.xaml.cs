125ef4daa92791bc39f2237cff5ef5e8561ab0a7
ysoup26 <ysoup26@gmail.com>
2024-10-16 16:42:27 +0900

    페이지 구조 살짝 변경
    사이드바를 유지하고,
    거기서 모니터링 항목을 선택할 수 있도록

--- MonitoringGUI/MonitoringGUI/View/MenuPage.xaml.cs
+++ MonitoringGUI/MonitoringGUI/View/MenuPage.xaml.cs
@@ -34,7 +34,9 @@ namespace MonitoringGUI.View
                     NavigationService.Navigate(test_menu_page);
                     break;
                 case "monitoring":
-                    MonitoringMenuPage monitoring_menu_page = new MonitoringMenuPage();
+                    // MonitoringMenuPage monitoring_menu_page = new MonitoringMenuPage();
+                    MonitoringDetailPage monitoring_menu_page = new MonitoringDetailPage();
+
                     NavigationService.Navigate(monitoring_menu_page);
                     break;
                 default:
